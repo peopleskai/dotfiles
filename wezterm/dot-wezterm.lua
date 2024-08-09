cf02bff7e732f1adb3ead62055231114b32780e4
Siu-Tung Tony Yuen <yuenton@amazon.com>
2024-08-09 15:26:55 -0700

    Added initial wezterm config

---
+++ wezterm/dot-wezterm.lua
@@ -0,0 +1,15 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = "Tokyo Night"
+config.window_background_opacity = 0.95
+config.text_background_opacity = 1.0
+config.font = wezterm.font("FiraCode Nerd Font")
+
+-- and finally, return the configuration to wezterm
+return config
