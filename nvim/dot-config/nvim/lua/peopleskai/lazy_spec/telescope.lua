08c38cf4db44c437ca270e7fd84206c30eb9cdf8
Tony <peopleskai@Tonys-MacBook-Air.local>
2024-10-19 21:45:51 -0700

    Updated telescope keymaps

--- nvim/dot-config/nvim/lua/peopleskai/lazy_spec/telescope.lua
+++ nvim/dot-config/nvim/lua/peopleskai/lazy_spec/telescope.lua
@@ -6,26 +6,28 @@ return {
   },
 
   config = function()
-    require('telescope').setup({})
+    local actions = require('telescope.actions')
+    require('telescope').setup({
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-d>'] = actions.delete_buffer,
+          },
+          n = {
+            ['dd'] = actions.delete_buffer,
+          },
+        },
+      },
+    })
 
     local builtin = require('telescope.builtin')
-
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = '[S]earch [G]it Files' })
-    vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch [G]rep' })
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>sww', function()
-      local word = vim.fn.expand('<cword>')
-      builtin.grep_string({ search = word })
-    end)
-    vim.keymap.set('n', '<leader>swW', function()
-      local word = vim.fn.expand('<cWORD>')
-      builtin.grep_string({ search = word })
-    end)
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope [F]ind [F]iles' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope [F]ind with live [G]rep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope [F]ind [B]uffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope [F]ind [H]elp tags' })
+    vim.keymap.set('n', '<leader>*', builtin.grep_string, { desc = 'Telescope grep word under cursor' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope [F]ind [K]eymaps' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope [F]ind [D]iagnostics' })
+    vim.keymap.set('n', '<leader>ft', builtin.treesitter, { desc = 'Telescope [F]ind with [T]reesitter' })
   end,
 }
