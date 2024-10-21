3c3bc959086a62428b9ca6e40538980045a5d3ef
Tony <peopleskai@Tonys-MacBook-Air.local>
2024-10-20 19:45:21 -0700

    Cleaned up LSP config, added snippets and tools to pre-install

--- nvim/dot-config/nvim/lua/peopleskai/lazy_spec/lsp.lua
+++ nvim/dot-config/nvim/lua/peopleskai/lazy_spec/lsp.lua
@@ -1,20 +1,6 @@
-local function dump(o)
-  if type(o) == 'table' then
-    local s = '{ '
-    for k, v in pairs(o) do
-      if type(k) ~= 'number' then
-        k = '"' .. k .. '"'
-      end
-      s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-    end
-    return s .. '} '
-  else
-    return tostring(o)
-  end
-end
-
 return {
-  { -- LSP Configuration & Plugins
+  -- LSP Configuration & Plugins
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
@@ -27,31 +13,6 @@ return {
       { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
@@ -59,11 +20,6 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
-          -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself.
-          --
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
           local map = function(keys, func, desc)
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
@@ -139,6 +95,7 @@ return {
       local ensure_installed = {
         'gersemi',
         'prettier',
+        'prettierd',
         'clang-format',
         'bash-language-server',
         'clangd',
@@ -153,6 +110,8 @@ return {
         'shfmt',
         'stylua',
         'taplo',
+        'isort',
+        'black',
       }
 
       require('mason').setup()
@@ -204,9 +163,11 @@ return {
       require('lspconfig').ts_ls.setup({})
 
       -- Rust Analyzer Config
-      -- require('lspconfig').rust_analyzer.setup({
-      --   capabilities = capabilities,
-      -- })
+      require('lspconfig').rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+
+      -- Custom Rust config to work with Amazon cargo build system
       -- require('lspconfig').rust_analyzer.setup({
       --   capabilities = capabilities,
       -- settings = {
@@ -231,8 +192,8 @@ return {
       -- })
     end,
   },
-
-  { -- Autocompletion
+  -- Autocompletion
+  {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
@@ -248,17 +209,7 @@ return {
           end
           return 'make install_jsregexp'
         end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
+        dependencies = { 'rafamadriz/friendly-snippets' },
       },
       'saadparwaiz1/cmp_luasnip',
 
