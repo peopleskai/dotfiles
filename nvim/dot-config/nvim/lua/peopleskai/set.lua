0abb737973d5741d388a014d3c9ad2a6d80573c2
Siu-Tung Tony Yuen <yuenton@amazon.com>
2025-03-02 22:48:50 -0800

    [nvim, starship] reduce starship cmd timeout to prevent git lag

--- nvim/dot-config/nvim/lua/peopleskai/set.lua
+++ nvim/dot-config/nvim/lua/peopleskai/set.lua
@@ -49,7 +49,7 @@ vim.opt.scrolloff = 10
 -- Set highlight on search
 vim.opt.hlsearch = true
 
-vim.opt.colorcolumn = '100'
+vim.opt.colorcolumn = '150'
 
 -- Set completeopt to have a better completion experience
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
