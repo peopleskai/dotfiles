db050ec7b80bd644c0034c8ee5cd30d4da36dca3
Siu-Tung Tony Yuen <yuenton@amazon.com>
2025-03-26 14:15:28 -0700

    [nvim] add better markdown support, todo highlight, better bookmarks

--- nvim/dot-config/nvim/lua/peopleskai/lazy_spec/init.lua
+++ nvim/dot-config/nvim/lua/peopleskai/lazy_spec/init.lua
@@ -25,6 +25,33 @@ return {
       vim.fn['mkdp#util#install']()
     end,
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
+  -- Highlight TODOs
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+
+  -- Better nvim bookmark
+  {
+    'chentoast/marks.nvim',
+    event = 'VeryLazy',
+    opts = {},
+  },
 
   -- flutter tools
   {
