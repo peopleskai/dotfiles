04683ae3c539b396f2fe01ae462c30c8525e4848
Siu-Tung Tony Yuen <yuenton@amazon.com>
2025-08-03 22:46:23 -0700

    Fixed neovim markdown previewer plugin

--- nvim/dot-config/nvim/lua/peopleskai/lazy_spec/init.lua
+++ nvim/dot-config/nvim/lua/peopleskai/lazy_spec/init.lua
@@ -20,19 +20,11 @@ return {
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
     end,
-  },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+    ft = { 'markdown' },
   },
 
   -- Highlight TODOs
