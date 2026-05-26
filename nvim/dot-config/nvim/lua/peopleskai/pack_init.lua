71f17371e9f8e17b4ff0a0edd4eb5108fbab4109
Tony Yuen <yuenton@amazon.com>
2026-05-26 12:36:46 -0700

    Updated statusline to not truncate filename

--- nvim/dot-config/nvim/lua/peopleskai/pack_init.lua
+++ nvim/dot-config/nvim/lua/peopleskai/pack_init.lua
@@ -746,7 +746,32 @@ vim.keymap.set({ 'n', 'x' }, '<leader>ap', function() require("sidekick.cli").pr
 --------------------------------------------------------------------------------
 -- mini.statusline
 --------------------------------------------------------------------------------
-require('mini.statusline').setup()
+require('mini.statusline').setup({
+  content = {
+    active = function()
+      local MiniStatusline = require('mini.statusline')
+      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      local git           = MiniStatusline.section_git({ trunc_width = 40 })
+      local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+      local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+      local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+      local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+      local location      = MiniStatusline.section_location({ trunc_width = 75 })
+      local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+      return MiniStatusline.combine_groups({
+        { hl = mode_hl,                  strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
+        '%<',
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        '%=',
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl,                  strings = { search, location } },
+      })
+    end,
+  },
+})
 
 --------------------------------------------------------------------------------
 -- Plugin update keymap
